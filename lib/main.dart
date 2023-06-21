12e869c2678e979d99d3bb6525b2d04d7966c286
Mykola Zinchenko <froddod@gmail.com>
2023-06-21 12:19:21 +0300

    ex

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:ooo/presentation/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TODO App',
       theme: ThemeData(
-        primaryColor: Color.fromRGBO(255, 255, 255, 0.9),
+        primaryColor: const Color.fromRGBO(255, 255, 255, 0.9),
         colorScheme: ThemeData().colorScheme.copyWith(
-          secondary: Color.fromRGBO(97, 120, 201, 1),
+          secondary: const Color.fromRGBO(97, 120, 201, 1),
         ),
       ),
       home: HomePage(),
--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(255, 255, 255, 0.9),
-        title: Padding(
+        backgroundColor: const Color.fromRGBO(255, 255, 255, 0.9),
+        title: const Padding(
           padding: EdgeInsets.only(left: 16.0),
           child: Align(
             alignment: Alignment.centerLeft,
@@ -23,12 +25,12 @@ class HomePage extends StatelessWidget {
         ),
         actions: [
           Container(
-            padding: EdgeInsets.only(right: 16.06),
+            padding: const EdgeInsets.only(right: 16.06),
             child: TextButton(
               onPressed: () {
                 //хуйня будт работать
               },
-              child: Text(
+              child: const Text(
                 'Create',
                 style: TextStyle(
                   color: Color.fromRGBO(152, 154, 166, 1),
@@ -38,9 +40,7 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        child: Text('соси хуй ярославЧИК'),
-      ),
+      body: const Text('соси хуй ярославЧИК'),
     );
   }
 }
